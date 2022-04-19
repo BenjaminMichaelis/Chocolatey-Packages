d2d9d7ffd530cd71ae506c6c4c58e137f735ea63
Benjamin Michaelis <benjamin@michaelis.net>
2022-04-19 11:23:51 -0700

    Add silent arg

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType = 'EXE'
   url  = 'https://todoist.com/windows_app'
-  checksum      = '618F9D7F07D0AFBA7BA076714A2B541E2022643113DC9E2A7B0EAA940676F1E8'
-  checksumType  = 'sha256'
+  silentArgs = '/S'
   validExitCodes = @(0)
 }
+
+Install-ChocolateyPackage @packageArgs
