00c45768ac08381fd862a79340e6e66f5bc91ba0
Benjamin Michaelis <benjamin@michaelis.net>
2022-04-18 22:28:02 -0700

    Add Todoist base

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType = 'EXE'
+  url  = 'https://todoist.com/windows_app'
+  checksum      = '618F9D7F07D0AFBA7BA076714A2B541E2022643113DC9E2A7B0EAA940676F1E8'
+  checksumType  = 'sha256'
+  validExitCodes = @(0)
+}
